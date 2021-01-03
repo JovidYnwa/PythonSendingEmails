d613a3eac33d18b4a858be295c8bd2a89dbb411c
Jovid Nurov <jovid.9@gmail.com>
2021-01-03 20:28:58 +0500

    first commit sould add readme

---
+++ temporary_db.py
@@ -0,0 +1,36 @@
+'''
+In this file we will have all of out data in json format
+'''
+
+customers = """{
+   "customers": [
+      {
+         "id":"1",
+         "name": "Jovid",
+         "phone": "992907013487",
+         "email": "jovid.9@gmail.com"
+      },
+      {
+         "id":"2",
+         "name": "Firuz",
+         "phone": "9929010001000",
+         "email": "firuzdzhonsa@gmail.com"
+      }
+   ]   
+}
+"""
+products = """{
+   "products": [
+      {
+         "id":"1",
+         "name": "яблоко",
+         "product_price": "20"
+      },
+      {
+         "id":"2",
+         "name": "вишня",
+         "product_price": "40"
+      }
+   ]   
+}
+"""
